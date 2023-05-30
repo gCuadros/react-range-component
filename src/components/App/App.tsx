e21c84754b0fb883b61e42436acfee1f471322cf
Gonzalo C <gondev@MacBook-Pro-de-Gonzalo.local>
2023-05-31 00:41:57 +0200

    feat(chore): add next.js to app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./app.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.p}>
+      <p>
+        <span>Hello</span>
+      </p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import Home from "../components/App/App";
+
+const Page = () => <Home />;
+
+export default Page;
