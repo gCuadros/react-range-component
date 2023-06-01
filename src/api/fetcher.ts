19434fd28a66d85bb12c1127d1cb47b0abb6b0e9
Gonzalo C <gondev@MacBook-Pro-de-Gonzalo.local>
2023-06-01 06:04:29 +0200

    fix(api): fix url

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,7 +1,7 @@
 import { CustomError } from "./error";
 
 export const fetcher = async (url: string, init?: RequestInit) => {
-  const res = await fetch(`http://demo3434971.mockable.io${url}`, {
+  const res = await fetch(`https://demo3434971.mockable.io/${url}`, {
     ...init,
     headers: {
       Accept: "application/json",
