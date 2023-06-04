d422dbf40cab8476efa2818c12e41a0268c90500
Gonzalo C <gondev@MacBook-Pro-de-Gonzalo.local>
2023-06-04 22:54:29 +0200

    feat(test): add test to main component

---
+++ src/components/Range/Range.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import Range from "components/Range";
+
+describe("Range", () => {
+  test("Debería mostrar el componente SkeletonLine cuando isLoading es verdadero", () => {
+    const { container } = render(<Range isLoading={true} />);
+    expect(container.querySelector(".skeleton-line")).toBeInTheDocument();
+  });
+
+  test("Debería mostrar el componente RangeDynamic cuando hay 2 valores permitidos", () => {
+    const values = [1, 100];
+    render(<Range values={values} />);
+
+    const RangeDynamic = screen.getByTestId("range-dynamic");
+    expect(RangeDynamic).toBeInTheDocument();
+  });
+
+  test("Debería mostrar el componente RangeFixed cuando hay más de 2 valores permitidos", () => {
+    const values = [1, 10, 20, 30];
+    render(<Range values={values} />);
+
+    const RangeFixed = screen.getByTestId("range-fixed");
+    expect(RangeFixed).toBeInTheDocument();
+  });
+});
